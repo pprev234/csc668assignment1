6a4fa571ae976da5a338b1fec3762ecbc7bdf708
Paul <pprevide@pprevide-Latitude-E7240>
2016-02-10 15:55:44 -0800

    First commit during setup process

---
+++ src/csc668assignment1/Csc668assignment1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc668assignment1;
+
+/**
+ *
+ * @author pprevide
+ */
+public class Csc668assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
