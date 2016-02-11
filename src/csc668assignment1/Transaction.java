5768b7947805a742296b417c36299fc1b20a7808
anconaesselmann <axel@anconaesselmann.com>
2016-02-11 15:04:37 -0800

    Merge attempt 3: Fixed conflicts in Resources, UserInterface and Transaction

---
+++ src/csc668assignment1/Transaction.java
@@ -0,0 +1,36 @@
+/*
+ * Just a stub with getters and setters.
+ */
+package csc668assignment1;
+
+import csc668assignment1.Payments.Payment;
+
+/**
+ *
+ * @author 
+ */
+public class Transaction {
+    private String _name, _upc;
+    private int _quantity;
+    private Payment _payment;
+    public Transaction(String name, String upc, int quantity, Payment payment) {
+        _name = name;
+        _upc = upc;
+        _quantity = quantity;
+        _payment = payment;
+    }
+    public String getUpc() {
+        return _upc;
+    }
+    public int getQuantity() {
+        return _quantity;
+    }
+
+    public String getName() {
+        return _name;
+    }
+    
+    public Payment getPayment() {
+        return _payment;
+    }
+}
