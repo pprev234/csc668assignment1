28ff0a782394d2fd6e6d1da4e03416b7cc74176b
kbigleybodlak <hypnothistle@yahoo.com>
2016-02-10 23:49:28 -0800

    SalesLineItem now gets a productSpec to retrieve defining data about the items purchased and a productCatalog instance to find the productSpec forthe upc given to instantiate the object.

--- src/csc668assignment1/SalesLineItem.java
+++ src/csc668assignment1/SalesLineItem.java
@@ -11,26 +11,47 @@ package csc668assignment1;
  */
 public class SalesLineItem {
     private int quantity;
+    private ProductSpec productSpec;
     private String upc;
-    
+    private ProductCatalog  pc = ProductCatalog.getInstance();
     public SalesLineItem(){
         
     }
-    public SalesLineItem(String upc, int quantity){
+    public SalesLineItem(String upc, int quantity ){
         this.quantity = quantity;
         this.upc = upc;
+        this.productSpec = setProductSpec(upc);
     }
-    
+    /*ACCESSORS*/
     public int getQuantity(){
         return this.quantity;
     }
     public String getUpc(){
         return this.upc;
     }
+    public ProductSpec getProductSpec(){
+        if(productSpec!=null){
+            return productSpec;
+        }
+        else
+            return null;
+    }
+    public double getSubtotal(){
+        return (this.quantity * this.productSpec.getUnitPrice());
+    }
+    /*MUTATORS*/
     public void setQuantity(int q){
         this.quantity=q;
     }
     public void setUpc(String upc){
         this.upc=upc;
     }
+    /**
+     * uses the upc of the product to retrieve the description from the Catalog
+     * @param upc 
+     */
+    public void setProductSpec(String upc){
+        this.productSpec = this.pc.getProductSpec(upc);
+    }
+    
 }
