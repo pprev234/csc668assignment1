9545d88c508a6c494e4391bf6054672d44caeafb
truelj <truelj@hotmail.com>
2016-02-24 00:20:22 -0800

    no more I/O in post

--- src/csc668assignment1/Post.java
+++ src/csc668assignment1/Post.java
@@ -1,7 +1,10 @@
 /*
  * Author: Jie Li
  * Date: 2016/2/10
- * Purpose:1. To deal with transactions, print invoices and store invoices to sale log. 
+ * Description: This is a new Post that contain nothing about Input and Output. 
+ * Post will be responsible for dealing with each Transaction received from Store
+ * and make invoice returned to Store.
+ * 
  */
 package csc668assignment1;
 
@@ -9,37 +12,20 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Post {
-    private TransactionReader transactionReader;
-    private Transaction currentTrans;
+    private Transaction currentTransaction;
     private Invoice currentInvoice;
-    public Post(String transactionFile) throws FileNotFoundException{
-        this.transactionReader = new TransactionReader(transactionFile);     
+    public Post(){    
     }
-    public void execute() throws IOException{
-        while(transactionReader.hasMoreTransactions()){
-            this.currentTrans = transactionReader.getNextTransaction();
-            //below is for debugging
-            //System.out.println("printing transactoin items");
-            //this.currentTrans.printTransItems();
-            //System.out.println("");
-            //end debugging. 
-            //this.currentInvoice = new Invoice(this.currentTrans);
-            setInvoice();
-            currentInvoice.print();
-            System.out.println("");
-        }
+    public void processTransaction(Transaction newTransaction){
+        this.currentTransaction = newTransaction;
+        this.setInvoice();
     }
+  
     public void setInvoice(){
-        this.currentInvoice = new Invoice(currentTrans);
+        this.currentInvoice = new Invoice(this.currentTransaction);
     }
-    //for testing 
-    //Result: Post makes use of TransactionReader to read a list of transactions successfully.
-    public static void main(String[] args) throws FileNotFoundException, IOException{
-        ProductCatalog.getProductCatelog("products.txt").setProductCatelog();
-        Post p = new Post("Transactions.txt");
-        p.execute();
-        
-        
+    public Invoice getInvoice(){
+        return this.currentInvoice;
     }
     
     
