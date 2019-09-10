2d840c91f90e65745206d302f1a36e57a667cb43
pprevide <pprevide@yahoo.com>
2019-09-10 10:24:13 -0700

    Add new constructor to Customer class

--- src/records/Customer.java
+++ src/records/Customer.java
@@ -2,18 +2,20 @@
  * Author: Paul Previde
  * Customer class stores information relating to the customers who enter 
  * into transactions at the Post.
+ * For this project, the only material attribute of the Customer is the name.
  */
 package records;
 
 
 public class Customer {
-    
+
     private String name;
-    
-    public Customer () { }
-    
-    public Customer (String name) {
-        this.name=name;
+
+    public Customer() {
+    }
+
+    public Customer(String name) {
+        this.name = name;
     }
 
     public String getName() {
@@ -23,7 +25,6 @@ public class Customer {
     public void setName(String name) {
         this.name = name;
     }
-    
-    
-    
+
+
 }
