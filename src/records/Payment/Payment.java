ad8f08028fe4280224bb51592f90031905a2638b
pprevide <pprevide@yahoo.com>
2019-11-07 10:43:44 -0800

    Define abstract class for payment methods

--- src/records/Payment/Payment.java
+++ src/records/Payment/Payment.java
@@ -1,31 +1,32 @@
-/*
- * Only a stub for now
- */
+
 package records.Payment;
 
 /**
+ * Parent abstract class for subclasses corresponding to payment methods.
+ *
+ * Each child class that extends this class will implement the makePayment() method.
  *
  * @author axelanconaesselmann, Susanne
  */
-public abstract  class Payment {
+public abstract class Payment {
     protected double _amountDue;
     protected String _type;
-    
-    public Payment(){
+
+    public Payment() {
         _amountDue = 0.0;
         _type = "";
     }
-    
+
     public Payment(double amount) {
         _amountDue = amount;
     }
-    
+
     public abstract void makePayment(double amountPaid);
 
     public String getType() {
         return _type;
     }
-    
+
     public double getAmountDue() {
         return _amountDue;
     }
