05a44f647647543bd9fc744f471b02b3a5c39e24
pprevide <pprevide@yahoo.com>
2019-11-07 11:40:57 -0800

    Add SalesLog class for handling invoices

--- src/records/SalesLog.java
+++ src/records/SalesLog.java
@@ -1,33 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Enables the viewing of Invoices and recording of sales as invoices.
+ *
+ * This class provides convenience methods for handling a list of invoices.
+ *
+ * @author Karl
  */
+
 package records;
+
 import java.util.LinkedList;
 
 
-
-/**
- * allows the viewing of Invoices and recording of sales as invoices.
- * @author Karl
- */
 public class SalesLog {
     private LinkedList<Invoice> invoiceList;
-    SalesLog(){
-        //initialize the invoiceList
-        invoiceList = new LinkedList<Invoice>();
-    };
-    public void viewAllInvoices(){
 
+    SalesLog() {
+        invoiceList = new LinkedList<Invoice>();
     }
-    public void viewInvoice(String name){
-        
-    }
-    public void recordInvoice(Invoice t){
+
+    public void recordInvoice(Invoice t) {
         invoiceList.add(t);
     }
-    
-    
-    
+
 }
